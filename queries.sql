87fbb05bce836e4fe10c99089e1a655ffdaf00dc
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-08-21 14:37:40 +0300

    Add queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animal;
+
+SELECT name FROM animal WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
+
+SELECT name FROM animal WHERE neutered = true AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animal WHERE name IN ('Agumon', 'Pikachu');
+
+SELECT name, escape_attempts FROM animal WHERE weight_kg > 10.5;
+
+SELECT * FROM animal WHERE neutered = true;
+
+SELECT * FROM animal WHERE name != 'Gabumon';
+
+SELECT * FROM animal WHERE weight_kg BETWEEN 10.4 AND 17.3;
