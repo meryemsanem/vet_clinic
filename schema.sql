3e75121e47b38448cfd23a61830f51382c5af020
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-08-22 11:37:23 +0300

    Add species column to the table

--- schema.sql
+++ schema.sql
@@ -1,9 +1,11 @@
-/* Database schema to keep the structure of entire database. */
+
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     date_of_birth DATE,
     escape_attempts INTEGER,
     neutered BOOLEAN,
-    weight_kg DECIMAL
+    weight_kg DECIMAL,
 );
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(255);
